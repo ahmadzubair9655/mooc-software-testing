f7e89a7884d581ce83377e0645be2c2c55c90c95
Ahmad Zubair <ahmad.zubair.skipq@gmail.com>
2024-02-14 15:13:40 +0000

    Update GettingStartedTest.java
    We added the value of 5 in unit test 2 and wrote whole unit test 3 keeping in mind the name of method used + adding value in it

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -13,15 +13,17 @@ public class GettingStartedTest {
 
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
+    @Test
+    public void addFiveToZero() {
+//        int result = new GettingStarted().addFive(???);  we need to fill in some input
+        int result = new GettingStarted().addFive(0); // in above command we gave number 5
+//        Assertions.assertEquals(???, result);  what we are comparing it with we haven't defined here properly
+        Assertions.assertEquals(5, result); // we added number 5 to make comparison with result
+    }
 //
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
 }
